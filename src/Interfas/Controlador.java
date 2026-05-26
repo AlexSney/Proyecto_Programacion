f92672195453f3ed99e9b1d63d87897d9d736b23
jhajho-06 <jhoelagila06@gmail.com>
2026-05-25 23:16:14 -0500

    saldo v.0

--- src/Interfas/Controlador.java
+++ src/Interfas/Controlador.java
@@ -2,11 +2,14 @@ package Interfas;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import saldo.Saldo;
 
 public class Controlador {
     private Map<String, String> usuarios;
     private Scanner scanner;
     private String usuarioLogeado;
+
+    Saldo saldo = new Saldo();
 
     
     public Controlador() {
@@ -16,9 +19,9 @@ public class Controlador {
 /**
      *Aqui van a crear sus usuarios y contraseñas para que puedan iniciar sesión, pueden agregar los que quieran, pero al menos deben agregar uno para poder probar el sistema de login. 
      */
-        usuarios.put("Elpapu", "1234");
+        usuarios.put("Jhoel", "1234");
     }
-
+    
     public void iniciar() {
         boolean salir = false;
         while (!salir) {
@@ -82,8 +85,9 @@ public class Controlador {
         usuarios.put(user, pass);
         System.out.println("\n¡Registro completado con éxito! Ya puede iniciar sesión.");
     }
-
+    
     private void menuLogeado() {
+        int opcionS = 0;
         boolean cerrarSesion = false;
         while (!cerrarSesion) {
             System.out.println("\n---------------------------------");
@@ -96,6 +100,23 @@ public class Controlador {
             String opcion = scanner.nextLine();
             if (opcion.equals("1")) {
                 System.out.println("\nPerfil actual: " + usuarioLogeado);
+                System.out.println("Seleccione que desea hacer:");
+                System.out.println("1. Consultar saldo actual");
+                System.out.println("2. Agregar saldo");
+                System.out.println("3. Regresar");
+                opcionS = saldo.ingresarInt();
+                switch (opcionS) {
+                    case 1:
+                        saldo.mostrarSaldo();
+                        break;
+                    case 2:
+                        double newSaldo = 150;
+                        saldo.modificarSaldo(newSaldo, true);
+                        break;
+                    default:
+                        break;
+                }
+
             } else if (opcion.equals("2")) {
                 System.out.println("\nCerrando sesión de " + usuarioLogeado + "...");
                 usuarioLogeado = null;
---
+++ src/saldo/ControladorSaldo.java
@@ -0,0 +1,11 @@
+package saldo;
+
+public class ControladorSaldo {
+    Saldo saldo = new Saldo();
+    
+    public void controladorSaldo() {
+        saldo.ingresarInt();
+        saldo.mostrarSaldo();
+    }
+
+}
