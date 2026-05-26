5caca52576494fda96fe01e42ccba15709319d84
AlexSney <elpibefachero2006@gmail.com>
2026-05-25 20:14:40 -0500

    Esta es la primera version de la APLICACION DE JUEGOS tugamesti

---
+++ src/App.java
@@ -0,0 +1,7 @@
+import Interfas.Controlador;
+public class App {
+    public static void main(String[] args) {
+        Controlador sistema = new Controlador();
+        sistema.iniciar();
+    }
+}
